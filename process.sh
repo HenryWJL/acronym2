94beb47f7a7ff323dc9f8f89cd39338aa996a52a
Junlin Wang <12112921@mail.sustech.edu.cn>
2024-03-08 23:55:13 +0800

    Update process.sh

--- process.sh
+++ process.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
-manifold_dir=/home/wangjunlin/project/Manifold
-project_dir=/home/wangjunlin/project/Contact-TransGrasp
-log_path=/home/wangjunlin/project/log.txt
-outlier_path=/home/wangjunlin/project/outlier.txt
+### Need to modify
+acronym_dir=/.../acronym_2.0
+manifold_dir=/.../Manifold
+mesh_dir=/...
+###
+log_path=$acronym_dir/log.txt
+outlier_path=$acronym_dir/outlier.txt
 
-# generate watertight versions and simplify them
-echo "=> Generating and simplifying watertight versions of meshes..."
+# generate watertight versions and simplify
+echo "Generating simplified watertight versions..."
 cd $manifold_dir/build
-for file in $project_dir/data/meshes/objects/*.obj
+for file in $mesh_dir/*.obj
 do
     ./manifold $file $file -s >> $log_path
     ./simplify -i $file -o $file -m -r 0.02 >> $log_path
 done
 
-# find outliers
-echo "=> Finding outliers..."
-cd $project_dir/dataset
-python3 find_outlier.py --log_path $log_path --outlier_save_path $outlier_path
+# search outliers
+echo "Searching outliers..."
+cd $acronym_dir
+python find_outlier.py --log_path $log_path --outlier_save_path $outlier_path
 
 # remove outliers
-echo "=> Removing outliers..."
-cd $project_dir/data/meshes/objects
+echo "Removing outliers..."
+cd $mesh_dir
 while read line
 do
     rm ./$line*.obj
@@ -32,3 +35,7 @@ while read line
 do
     rm ./*$line*.h5
 done <  $outlier_path
+
+echo "Completed"
+rm -f $log_path
+rm -f $outlier_path
